5a3416d09d092bae78da058d3e340b478b9d9ef5
Nico <nicolas.gastellu@mail.mcgill.ca>
2023-01-30 22:09:04 -0500

    Debugging `graph_tools.comoponents`: Fixed singlet bug.

--- qcnico/tests/components_test.py
+++ qcnico/tests/components_test.py
@@ -4,6 +4,22 @@ import numpy as np
 from qcnico import graph_tools as gt
 
 
-M = np.array((5,5),dtype=bool)
+M = np.zeros((12,12),dtype=bool)
 
-cluster1 = 
+neighbour_inds = np.array( [ (0,1),
+        (0,3),
+        (1,2),
+        (2,3),
+        (3,4),
+        (4,5),
+        (6,7),
+        (9,10),
+        (10,11) ] ).T
+neighbour_inds = tuple( tuple(n) for n in neighbour_inds )
+
+M[neighbour_inds] = True
+
+M += M.T
+
+c = gt.components(M)
+print(c)
