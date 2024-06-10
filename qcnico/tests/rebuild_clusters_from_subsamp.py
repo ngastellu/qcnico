a28320f717e4aba8b54209e495f2089ff0c341c2
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-06-10 01:02:51 -0400

    Bug fixes

--- qcnico/tests/rebuild_clusters_from_subsamp.py
+++ qcnico/tests/rebuild_clusters_from_subsamp.py
@@ -17,11 +17,11 @@ print(f'Initialising hash maps (m,n) = ({m,n})',flush=True)
 pos_global = {tuple(r):k for k,r in enumerate(np.load(f'hex_centers-{nn}_{m}_{n}.npy'))} # global hashtable mapping hexagin centers to integer indices
 
 M = np.load(f'M_hex-{nn}_{m}_{n}.npy')
-neighb_list = {k:tuple(M[k,:].nonzero()[0]) for k in M.shape[0]}
+neighb_list = {k:tuple(M[k,:].nonzero()[0]) for k in range(M.shape[0])}
 
 ncentres_tot = M.shape[0]
 
-assert pos_global.shape[0] == ncentres_tot, f'Mismatch between number of centers ({pos_global.shape[0]}) and shape of hexagon adjacency matrix {M.shape}!'
+assert len(pos_global) == ncentres_tot, f'Mismatch between number of centers ({pos_global.shape[0]}) and shape of hexagon adjacency matrix {M.shape}!'
 print('Done! Commencing loop over other subsamples...',flush=True)
 
 
@@ -29,6 +29,7 @@ for mn in slice_inds[1:]:
     m,n = mn
     print(f'\n------ {(m,n)} ------',flush=True)
     pos = np.load(f'hex_centers-{nn}_{m}_{n}.npy')
+    print(f'{pos.shape[0]} distinct crystalline centers.', flush=True)
     local_map = {k:-1 for k in range(pos.shape[0])} # hashtable that maps centre indices local to the NPY being processed to their global index (i.e. in `pos_global`)  
 
     print('Loop 1: ', end='')
@@ -36,24 +37,27 @@ for mn in slice_inds[1:]:
     for k, r in enumerate(pos):
         r = tuple(r)
         if r in pos_global:
+            #print(f'* {r} in pos_global *', flush=True)
             local_map[k] = pos_global[r]
         else:
+            #print(f'~ Adding {r} to pos_global ~', flush=True)
             pos_global[r] = ncentres_tot
             local_map[k] = ncentres_tot
             ncentres_tot += 1
     print('Done!',flush=True)
+    vals = np.array(local_map.values())
 
     print('Loop 2: ', end='',flush=True)
     # next, update neighbour list using global hashmap
     M = np.load(f'M_hex-{nn}_{m}_{n}.npy')
     for k in range(pos.shape[0]):
         k_global = local_map[k]
-        ineighbs_local = tuple(M.nonzero()[0])
+        ineighbs_local = tuple(M[:,k].nonzero()[0])
         # handle case 
         if k_global in neighb_list:
-            neighb_list[k_global] = neighb_list[k_global] + ineighbs_local
+            neighb_list[k_global] = neighb_list[k_global] + tuple(local_map[p] for p in ineighbs_local)
         else:
-            neighb_list[k_global] = ineighbs_local
+            neighb_list[k_global] = tuple(local_map[p] for p in ineighbs_local)
     print('Done!',flush=True)
 
 
@@ -66,12 +70,14 @@ isweird = np.zeros(ncentres_tot,dtype=bool)
 for k in range(ncentres_tot):
     ineighbs = neighb_list[k]
     Mglobal[k,ineighbs] = True
+    Mglobal[ineighbs,k] = True
 
-    nb_neighbs = len(ineighbs)
+    nb_neighbs = np.unique(ineighbs).shape[0]
     if nb_neighbs == 6:
          isnucleus[k] = True
     elif nb_neighbs > 6:
         isweird[k] = True
+
 print('Done! Saving stuff.', flush=True)
 
 np.save('Mhex_global-{nn}.npy',Mglobal)
@@ -83,21 +89,26 @@ with open('neighbs_dict.pkl', 'wb') as fo:
     pickle.dump(neighb_list,fo)
 
 nuclei = isnucleus.nonzero()[0]
-print(f'*** Found {nuclei.shape[0]} crystalline nuclei ***')
+print(f'*** Found {nuclei.shape[0]} crystalline nuclei ***', flush=True)
 
 if isweird.sum() > 0:
     weird = isweird.nonzero()[0]
-    print(f'!!!! Found{weird.shape[0]} weird nuclei !!!!')
+    print(f'!!!! Foundi {weird.shape[0]} weird nuclei !!!! Printing their number of neighbours now: ', flush=True)
+    for w in weird:
+        print(f'{w} --> {Mglobal[w,:].sum()}', flush=True)
 
 
 print('Searching for clusters...',flush=True)
 Mglobal = Mglobal.astype(np.int8)
-nuclei_neighbs = Mglobal[:,nuclei]
+nuclei_neighbs = Mglobal[:,nuclei].nonzero()[0]
 Mglobal2 = Mglobal @ Mglobal
 nuclei_next_neighbs = Mglobal2[:,nuclei].nonzero()[0]
-strict_6c = set(np.concatenate(nuclei,nuclei_neighbs,nuclei_next_neighbs))
+strict_6c = set(np.concatenate((nuclei,nuclei_neighbs,nuclei_next_neighbs)))
 crystalline_clusters = get_clusters(nuclei, Mglobal, strict_6c)
 print('Done!',flush=True)
 
 cluster_sizes = np.array([len(c) for c in crystalline_clusters])
 np.save(f'cryst_cluster_sizes-{nn}.npy',cluster_sizes)
+
+with open(f'clusters-{nn}.pkl', 'wb') as fo:
+    pickle.dump(crystalline_clusters,fo)
