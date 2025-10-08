b7028cf7b939ee592b1cb599e016226f680e72d0
Nico <nicolas.gastellu@mail.mcgill.ca>
2025-10-08 13:10:35 -0400

    Overhauled `md_utils.parse_log_LAMMPS`; now only accepts a single arg (path to LAMMPS log file) and parses using calls to `grep` through the `subprocess` module.

--- qcnico/md_utils.py
+++ qcnico/md_utils.py
@@ -3,22 +3,25 @@
 import numpy as np
 import subprocess as sbp
 
+def _get_nframes_LAMMPS_log(logfile):
+    """Counts the number of frames captured in the LAMMPS logfile."""
 
-def parse_LAMMPS_log(logfile,nsteps,fields,nb_opening_lines=29):
-    """Read data contained in the columns index by the `fields` array of LAMMPS log file.
-    `nsteps` must include step 0
-    Assumes the first `nb_opening_lines` do not contain relevant data."""
+    iframe0, iframe1 = [int(s.split()[0]) for s in sbp.run(f"grep -E '^[[:blank:]]+[0-9]+[[:blank:]]+[-]*[0-9]+' -m 2 {logfile}", shell=True, capture_output=True).stdout.decode().split('\n')[:-1]]
 
-    out_data = np.zeros((nsteps, len(fields)))
-    with open(logfile) as fo:
-        for n in range(nb_opening_lines):
-            fo.readline()
-    
-        for k in range(nsteps):
-            l = fo.readline().strip()
-            out_data[k,:] = np.array([float(s) for s in l.split()])[fields]
-    
-    return out_data
+    step = iframe1 - iframe0
+
+    iframe_last = int(sbp.run(f"tail -r {logfile} | grep -E '^[[:blank:]+[0-9]+[[:blank:]]+[-]*[0-9]+' -m 1", shell=True, capture_output=True).stdout.decode().split()[0])
+
+    return 1 + (iframe_last - iframe0) // step
+
+def parse_LAMMPS_log(logfile):
+    """Reads data written to a logfile by a `thermo` command during a LAMMPS MD run. 
+    Stores the output as dictionary whose keys are the column headers and the values are NumPy arrays containing
+    the different time series recorded during the MD run."""
+    cols = sbp.run(f"grep -E -m 1 '^[[:blank:]]+Step' {logfile}", shell=True, capture_output=True).stdout.decode().split()
+    data = sbp.run(f"grep -E '^[[:blank:]]+[0-9]+[[:blank:]]+[-]?[0-9]+' {logfile}", shell=True, capture_output=True).stdout.decode().split('\n')[:-1]
+    data = np.array([[float(l.split()[k]) for l in data] for k in range(len(cols))])
+    return {c:arr for c, arr in zip(cols, data)}
 
 
 def subsample_structure(pos,xlim=(-np.inf,np.inf),ylim=(-np.inf, np.inf)):
@@ -36,14 +39,16 @@ def count_natoms_lammpstrj(trajfile):
 
     
 def count_nsteps_lammpstrj(trajfile, Natoms=None, nb_context_lines=9):
+    """Gets index of last saved frame from LAMMPS dump file. This is usally equivalent to the number
+      of frames of the MD run it describes, except in the event the run's first frame is not indexed as 1."""
     if Natoms is None:
         Natoms = count_natoms_lammpstrj(trajfile)
 
     return int(sbp.run(f"tail -{Natoms + nb_context_lines} {trajfile} | grep -A 1 'TIMESTEP' | tail -1", shell=True, capture_output=True ).stdout.decode())
 
+
 def get_framerate_lammpstrj(trajfile, return_first_frame_index=False):
     cmd_out = sbp.run(f"grep -m 2 -A 1 'TIMESTEP' {trajfile}", shell=True, capture_output=True).stdout.decode().split('\n')
-    print(cmd_out)
     iframe0 = int(cmd_out[1])
     iframe1 = int(cmd_out[4])
     framerate = iframe1 - iframe0
